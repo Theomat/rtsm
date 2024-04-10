99626c8282ae405ac489be9f6a8de1b8a050b432
Theomat <theomatricon@gmail.com>
2024-04-10 15:33:30 +0200

    formatting

--- rtsm/__main__.py
+++ rtsm/__main__.py
@@ -111,7 +111,9 @@ if __name__ == "__main__":
         else:
             best_cost = min(sol.cost() for sol in solutions)
             print(f"found {F.GREEN}{len(solutions)}{F.RESET} solutions")
-            print(f"the minimal cost solution found is {F.GREEN}{best_cost}{F.RESET} ({F.GREEN}{best_cost/ len(instance.tests):.2%}{F.RESET})")
+            print(
+                f"the minimal cost solution found is {F.GREEN}{best_cost}{F.RESET} ({F.GREEN}{best_cost/ len(instance.tests):.2%}{F.RESET})"
+            )
 
     # Save solution
     with open(args.output, "w") as fd:
