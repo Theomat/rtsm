babdd38ed6b27d032627ab74550f3df79d0085e5
Theomat <theomatricon@gmail.com>
2024-04-12 11:41:58 +0200

    Added two methods to_ranking, ranking_error

--- rtsm/predictors/predictor.py
+++ rtsm/predictors/predictor.py
@@ -1,7 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import Tuple
 
+import numpy as np
+
 from rtsm.instance import Instance
+
+
+def to_ranking(perfs: np.ndarray) -> np.ndarray:
+    """
+    Transforms a performance vector (variant) into a ranking matrix.
+    """
+    n = perfs.shape[0]
+    return np.broadcast_to(perfs, (n, n)).T > perfs
+
+
+def ranking_error(target_ranks: np.ndarray, pred_ranks: np.ndarray) -> float:
+    """
+    Takes two ranking matrices and returns the percentage of errors.
+    The percentage of errors is percentage of order relations that were different.
+    """
+    n = target_ranks.shape[0]
+    num = np.sum(target_ranks != pred_ranks) - n
+    if num <= 0:
+        return 0
+    return num / (np.prod(target_ranks.shape) - n)
 
 
 class Predictor(ABC):
