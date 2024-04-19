46c08ff8d8f0e6f50ce8c8e594fcb0599dfd1917
Theomat <theomatricon@gmail.com>
2024-04-19 15:23:33 +0200

    Implemented export_prediction for linear

--- rtsm/predictors/linear_predictor.py
+++ rtsm/predictors/linear_predictor.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import json
 
 from rtsm.instance import Instance
 from rtsm.predictors.predictor import Predictor, to_ranking, ranking_error
@@ -7,10 +8,12 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
-def __learn_linear_model__(A: np.ndarray, y: np.ndarray) -> np.ndarray:
+def __learn_linear_model__(
+    A: np.ndarray, y: np.ndarray
+) -> Tuple[LinearRegression, np.ndarray]:
     model = LinearRegression()
     model.fit(A, y)
-    return model.predict(A)
+    return model, model.predict(A)
 
 
 class LinearRegressionPredictor(Predictor):
@@ -36,5 +39,26 @@ class LinearRegressionPredictor(Predictor):
         Y = self.Yt[:, [not x for x in usable]]
         D = np.sum(self.Xt, axis=-1)
         for i in range(Y.shape[1]):
-            D += __learn_linear_model__(X, Y[:, i].reshape((-1)))
+            D += __learn_linear_model__(X, Y[:, i].reshape((-1)))[1]
         return ranking_error(self.Rt, to_ranking(D))
+
+    def export_prediction(self, usable: Tuple[bool], path: str) -> None:
+        out = {
+            "type": "linear",
+            "input": self.instance.get_tests(usable),
+            "output": [t for t, b in zip(self.instance.tests, usable) if not b],
+            "coefficients": [],
+            "translation": [],
+        }
+        X = self.Xt[:, usable]
+        Y = self.Yt[:, [not x for x in usable]]
+        coeffs = np.zeros((X.shape[1], Y.shape[1]))
+        intercept = np.zeros((Y.shape[1]))
+        for i in range(Y.shape[1]):
+            model = __learn_linear_model__(X, Y[:, i].reshape((-1)))[0]
+            coeffs[:, i] = model.coef_
+            intercept[i] = model.intercept_
+        out["coefficients"] = coeffs.tolist()
+        out["translation"] = intercept.tolist()
+        with open(path, "w") as fd:
+            json.dump(out, fd)
