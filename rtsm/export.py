8f67e96df6e263a62d216e904b0533e90deffed5
Theomat <theomatricon@gmail.com>
2024-04-22 13:45:38 +0200

    Added linear+

--- rtsm/export.py
+++ rtsm/export.py
@@ -32,6 +32,9 @@ if __name__ == "__main__":
         "logistic-bool": LogisticBooleanPredictor,
         "logistic-rank": LogisticRankPredictor,
         "linear": LinearRegressionPredictor,
+        "linear+": lambda x, **kwargs: LinearRegressionPredictor(
+            x, positive=True, **kwargs
+        ),
     }
 
     # Data Loaders
