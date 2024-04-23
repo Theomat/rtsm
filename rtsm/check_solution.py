9a226e73f5dad6d5666ace3484e0ec530ab7d638
Theomat <theomatricon@gmail.com>
2024-04-23 11:14:56 +0200

    Added spearman correlation coefficient

--- rtsm/check_solution.py
+++ rtsm/check_solution.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import numpy as np
     from colorama import Fore as F
 
-    from rtsm.predictors.predictor import Predictor, to_ranking, ranking_error
+    from rtsm.predictors.predictor import Predictor, to_ranking, ranking_error, to_ranks
 
     from rtsm.helper import get_predictors, get_data_loaders
 
@@ -82,15 +82,33 @@ if __name__ == "__main__":
     print(f"prediction model: {F.CYAN}{predictor.get_name()}{F.RESET}")
 
     R = to_ranking(np.sum(instance.performance_matrix, axis=-1))
+    ranks_original = to_ranks(R)
+
+    n = len(instance.variants)
+
     sR = to_ranking(np.sum(instance.subset(one_sol).performance_matrix, axis=-1))
-    print(
-        f"ranking error without prediction: {F.GREEN}{ranking_error(R, sR):.2%}{F.RESET}"
-    )
+    ranks_no_pred = to_ranks(sR)
+    print("without prediction:")
+    print(f"\tranking error: {F.GREEN}{ranking_error(R, sR):.2%}{F.RESET}")
+
+    def spearman_to_str(rankA, rankB):
+        r_s = 1 - 6 * np.sum(np.square(rankA - rankB), axis=1) / ((n * n - 1) * n)
+        return " ".join(
+            f"{perf}:{F.GREEN}{score:.3f}{F.RESET}"
+            for perf, score in zip(instance.performances, r_s)
+        )
+
+    spearman_desc = spearman_to_str(ranks_original, ranks_no_pred)
+    print(f"\tspearman: {spearman_desc}")
+    print("with prediction:")
+
     start = time.perf_counter()
-    error = ranking_error(
-        R, predictor.get_ranking([x in one_sol for x in instance.tests])
-    )
+    ranking_pred = predictor.get_ranking([x in one_sol for x in instance.tests])
     duration = time.perf_counter() - start
+    error = ranking_error(R, ranking_pred)
+    ranks_pred = to_ranks(ranking_pred)
     print(
-        f"ranking error with prediction ({F.LIGHTYELLOW_EX}{duration:.2f}{F.RESET}s): {F.GREEN}{error:.2%}{F.RESET}"
+        f"\tranking error ({F.LIGHTYELLOW_EX}{duration:.2f}{F.RESET}s): {F.GREEN}{error:.2%}{F.RESET}"
     )
+    spearman_desc = spearman_to_str(ranks_original, ranks_pred)
+    print(f"\tspearman: {spearman_desc}")
