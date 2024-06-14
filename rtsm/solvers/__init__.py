f48e098c030c7b2518745e117b638c9873cfdea2
Theomat <theomatricon@gmail.com>
2024-06-14 08:38:07 +0200

    Added guard when pygad is not installed

--- rtsm/solvers/__init__.py
+++ rtsm/solvers/__init__.py
@@ -3,4 +3,8 @@ from rtsm.solvers.bisect_solver import BisectSolver
 from rtsm.solvers.ri_solver import RandomImprovementSolver
 from rtsm.solvers.rs_solver import RandomSolutionSolver
 from rtsm.solvers.fusion_solver import FusionSolver
-from rtsm.solvers.ga_solver import GASolver
+
+try:
+    from rtsm.solvers.ga_solver import GASolver
+except:
+    pass
