8fa971e5ed646f4c8205c3c5f59582e209941ee5
Theomat <theomatricon@gmail.com>
2024-10-09 14:41:29 +0200

    Refactoring

---
+++ rtsm/solvers/deterministic_solver.py
@@ -0,0 +1,82 @@
+from typing import Callable, Optional, Set, Tuple, Union
+
+import numpy as np
+
+from abc import abstractmethod, ABC
+
+from rtsm.instance import Instance
+from rtsm.predictors.predictor import Predictor
+from rtsm.solution import Solution
+from rtsm.solvers.solver import Solver
+from rtsm.utils.color_helper import get_color_helper
+from rtsm.utils.progress_bar import ProgressBar
+
+F = get_color_helper()
+
+
+def __new_sol__(
+    sol: np.ndarray,
+    current_best: int,
+    pbar: ProgressBar,
+):
+    score = np.sum(sol)
+    if score < current_best:
+        pbar.set_best(score, score / len(sol))
+        return score
+    return current_best
+
+
+class DeterministicSolver(Solver, ABC):
+    def solve(
+        self,
+        instance: Instance,
+        predictor_builder: Union[Callable[[Instance], Predictor], Predictor],
+        use_tqdm: bool = False,
+        verbose: bool = False,
+        **kwargs,
+    ) -> Set[Solution]:
+        """
+        Try to solve an instance of RTSM and provides a set of solutions.
+        """
+        predictor = (
+            predictor_builder(instance)
+            if not isinstance(predictor_builder, Predictor)
+            else predictor_builder
+        )
+        self.instance = instance
+        init = np.asarray(instance.warm_start())
+        self.best_sol = [init]
+        best_cost = np.sum(init)
+        if verbose:
+            print(
+                f"{self._get_print_prefix_()}{F.LIGHTCYAN_EX}[info]{F.RESET} init: {F.LIGHTCYAN_EX}{best_cost}{F.RESET} ({F.LIGHTCYAN_EX}{best_cost / len(init):.1%}{F.RESET})"
+            )
+
+        pbar = ProgressBar(total=best_cost, name=self.get_name(), use_tqdm=use_tqdm)
+        allowed = np.copy(init)
+        for _ in range(best_cost):
+            to_remove_index = self.__choose_index_to_remove__(allowed, predictor)
+            allowed[to_remove_index] = False
+            if predictor.can_predict(allowed):
+                best_cost = __new_sol__(allowed, best_cost, pbar)
+            else:
+                break
+            pbar.update(1)
+        pbar.close()
+
+        return self.__get_solutions__()
+
+    def __get_solutions__(self) -> Set[Solution]:
+        return {
+            Solution(self.instance, tuple(self.instance.get_tests(sol)))
+            for sol in self.best_sol
+        }
+
+    @abstractmethod
+    def __choose_index_to_remove__(
+        self, current: np.ndarray, predictor: Predictor
+    ) -> int:
+        pass
+
+    def early_exit(self) -> Set[Solution]:
+        return self.__get_solutions__()
--- rtsm/solvers/greedy_solver.py
+++ rtsm/solvers/greedy_solver.py
@@ -1,30 +1,10 @@
-from typing import Callable, Optional, Set, Tuple, Union
-
 import numpy as np
 
-from rtsm.instance import Instance
 from rtsm.predictors.predictor import Predictor
-from rtsm.solution import Solution
-from rtsm.solvers.solver import Solver
-from rtsm.utils.color_helper import get_color_helper
-from rtsm.utils.progress_bar import ProgressBar
-
-F = get_color_helper()
+from rtsm.solvers.deterministic_solver import DeterministicSolver
 
 
-def __new_sol__(
-    sol: np.ndarray,
-    current_best: int,
-    pbar: ProgressBar,
-):
-    score = np.sum(sol)
-    if score < current_best:
-        pbar.set_best(score, score / len(sol))
-        return score
-    return current_best
-
-
-class GreedySolver(Solver):
+class GreedySolver(DeterministicSolver):
     """
     A solver that greedily chooses tests.
 
@@ -33,58 +13,16 @@ class GreedySolver(Solver):
     def get_name(self) -> str:
         return "greedy"
 
-    def solve(
-        self,
-        instance: Instance,
-        predictor_builder: Union[Callable[[Instance], Predictor], Predictor],
-        use_tqdm: bool = False,
-        verbose: bool = False,
-        **kwargs,
-    ) -> Set[Solution]:
-        """
-        Try to solve an instance of RTSM and provides a set of solutions.
-        """
-        predictor = (
-            predictor_builder(instance)
-            if not isinstance(predictor_builder, Predictor)
-            else predictor_builder
-        )
-        self.instance = instance
-        init = np.asarray(instance.warm_start())
-        self.best_sol = [init]
-        best_cost = np.sum(init)
-        if verbose:
-            print(
-                f"{self._get_print_prefix_()}{F.LIGHTCYAN_EX}[info]{F.RESET} init: {F.LIGHTCYAN_EX}{best_cost}{F.RESET} ({F.LIGHTCYAN_EX}{best_cost / len(init):.1%}{F.RESET})"
-            )
-
-        pbar = ProgressBar(total=best_cost, name=self.get_name(), use_tqdm=use_tqdm)
-        allowed = np.copy(init)
-        for _ in range(best_cost):
-            mini = float("inf")
-            mini_index = 0
-            costs = np.sum(instance.performance_matrix[:, :, allowed], axis=(0, 1))
-            j = 0
-            for x in allowed:
-                if x and costs[j] < mini:
-                    mini = costs[j]
-                    mini_index = j
-                j += x
-            allowed[mini_index] = False
-            if predictor.can_predict(allowed):
-                best_cost = __new_sol__(allowed, best_cost, pbar)
-            else:
-                break
-            pbar.update(1)
-        pbar.close()
-
-        return self.__get_solutions__()
-
-    def __get_solutions__(self) -> Set[Solution]:
-        return {
-            Solution(self.instance, tuple(self.instance.get_tests(sol)))
-            for sol in self.best_sol
-        }
-
-    def early_exit(self) -> Set[Solution]:
-        return self.__get_solutions__()
+    def __choose_index_to_remove__(
+        self, current: np.ndarray, predictor: Predictor
+    ) -> int:
+        mini = float("inf")
+        mini_index = 0
+        costs = np.sum(self.instance.performance_matrix[:, :, current], axis=(0, 1))
+        j = 0
+        for x in current:
+            if x and costs[j] < mini:
+                mini = costs[j]
+                mini_index = j
+            j += x
+        return mini_index
