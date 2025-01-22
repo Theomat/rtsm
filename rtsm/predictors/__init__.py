55c0c4a9d717cf0e7397ffeb450662bbd3dc0be6
Theomat <theomatricon@gmail.com>
2025-01-22 13:25:20 +0100

    disable useless check for 20% perf. gain

--- rtsm/predictors/__init__.py
+++ rtsm/predictors/__init__.py
@@ -2,3 +2,9 @@ from rtsm.predictors.predictor import Predictor
 from rtsm.predictors.no_predictor import NoPredictor
 from rtsm.predictors.logistic_boolean_predictor import LogisticBooleanPredictor
 from rtsm.predictors.linear_predictor import LinearRegressionPredictor
+import sklearn
+
+sklearn.set_config(
+    assume_finite=True,  # disable validation
+    skip_parameter_validation=True,
+)
