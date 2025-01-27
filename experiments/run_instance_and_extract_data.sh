d47d4b5375fd84c4b8d571bee77a44db1a17cfc9
Theomat <theomatricon@gmail.com>
2025-01-27 10:38:46 +0100

    support kendall and MILP

--- experiments/run_instance_and_extract_data.sh
+++ experiments/run_instance_and_extract_data.sh
@@ -1,18 +1,26 @@
 #ARGS:
 # 1: seed
-# 2: solver
-# 3: instance file
-# 4: full instance file
-# 5: benchmark name
-# 6: DST folder without the end /
-FOLDER=$6
-SOL_FILE="$FOLDER/$5.json"
-DATA_FILE="$FOLDER/$5.tmp"
+# 2: kendall
+# 3: solver
+# 4: instance file
+# 5: full instance file
+# 6: benchmark name
+# 7: DST folder without the end /
+FOLDER=$7
+SOL_FILE="$FOLDER/$6.json"
+DATA_FILE="$FOLDER/$6.tmp"
 if [ ! -f $SOL_FILE ]; then
-    timeout 3600 python -m rtsm.crunch --predictor weighted --seed $1 --solver $2 -o $SOL_FILE $3
+    if [ "$3" = "MILP" ]; then
+        timeout 3000 python -m rtsm.milp -o $SOL_FILE $4
+    else 
+        timeout 3000 python -m rtsm.crunch --predictor weighted --seed $1 --solver $3 -o $SOL_FILE --kendall $2 $4
+    fi
+    if [ ! -s $SOL_FILE ]; then
+        python experiments/default_solution.py --predictor weighted --seed $1 --solver $3 -o $SOL_FILE --kendall $2 $4 -t 3000
+    fi
 fi
 if [ ! -f "$DATA_FILE" ]; then
-    python -m rtsm.check_solution $3 $SOL_FILE --full $4 > $DATA_FILE || rm $DATA_FILE
+    python -m rtsm.check_solution $4 $SOL_FILE --full $5 > $DATA_FILE || rm $DATA_FILE
     if [ -f $DATA_FILE ]; then
         runtime=$(cat $SOL_FILE | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["runtime"])')
         size=$(cat $SOL_FILE | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(len(obj["solutions"][0]))')
