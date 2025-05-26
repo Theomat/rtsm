fc05bc0d38d1cecb71a15b26d7e099d26a21d6bb
Théo Matricon <theomatricon@gmail.com>
2025-05-26 13:29:54 +0200

    fixed issues and added variants,tests

--- experiments/data_aggregator.py
+++ experiments/data_aggregator.py
@@ -1,17 +1,38 @@
 from collections import defaultdict
+import json
 import sys
 import glob
 
-COLUMNS = "fraction,partition_seed,seed,solver,target_kendall,kendall,spearman,size,runtime,cost,total_cost".split(
+COLUMNS = "fraction,partition_seed,seed,solver,target_kendall,kendall,spearman,size,runtime,cost,total_cost,variants,tests".split(
     ","
 )
+
+
+def is_float(x) -> bool:
+    try:
+        float(x)
+    except ValueError:
+        return False
+    return True
+
+
+def get_name(idx) -> str:
+    return COLUMNS[idx]
+
+
+def fix_size_and_runtime(file: str) -> tuple[int, float]:
+    with open(file.replace(".tmp", ".json")) as fd:
+        data = json.load(fd)
+        runtime = float(data["runtime"])
+        size = len(list(data["solutions"]).pop())
+    return size, runtime
 
 
 def read_file(file: str) -> dict:
     parts = [x for x in file.split(".")[:-1] if len(x) > 0]
     filename = parts[0]
     filename = filename[filename.rfind("/") + 1 :]
-    out = {}
+
     data = []
     fraction = parts[-4]
     data.append(fraction if fraction != "5" else "50")
@@ -20,12 +41,10 @@ def read_file(file: str) -> dict:
     data.append(parts[-2])
     data.append(parts[-1])
     with open(file) as fd:
-        lines = fd.readlines()
+        lines = [x.strip("\n ") for x in fd.readlines() if len(x.strip("\n ")) > 0]
         if len(lines) <= 2:
             print("skipping:", file)
             return {}
-        out["runtime"] = lines[-2]
-        out["size"] = lines[-1]
         kendalls = [
             line[line.index(":") + 1 :].strip() for line in lines if "Kendall" in line
         ]
@@ -42,8 +61,13 @@ def read_file(file: str) -> dict:
         spearman = worst
         data.append(kendall)
         data.append(spearman)
-        data.append(lines[-1][:-1])
-        data.append(lines[-2][:-1])
+        data.append(lines[-1])  # size
+        data.append(lines[-2])  # runtime
+        if "\t" in data[-1] or "\t" in data[-2]:
+            # We are missing size or runtime so fix this
+            size, runtime = fix_size_and_runtime(file)
+            data[-2] = size
+            data[-1] = runtime
         cost = [
             line[line.index(":") + 1 :].strip() for line in lines if "cost:" in line
         ][0]
@@ -51,6 +75,14 @@ def read_file(file: str) -> dict:
         total_cost = cost[cost.index("/") + 1 : cost.index("(")].strip()
         data.append(sol_cost)
         data.append(total_cost)
+        parts = lines[0].split(" ")
+        data.append(parts[1])
+        data.append(parts[4])
+        for i in range(-8, 0):
+            assert is_float(data[i]), (
+                f"failed parsing [{get_name(i)}]:{file} = <<{data[i]}>>"
+            )
+
     return {"file": filename, "data": data}
 
 
