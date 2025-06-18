0484dda52a7bda0a339b1c0ee8e418602b6be602
Théo Matricon <theomatricon@gmail.com>
2025-06-18 13:47:49 +0200

    high quality plotting

--- experiments/analysis/produce_dist.py
+++ experiments/analysis/produce_dist.py
@@ -1,23 +1,31 @@
-# PROUT /// script
+# /// script
 # dependencies = [
 #   "pandas",
 #   "seaborn",
 #   "pltpublish",
-#   "tqdm"
+#   "scipy"
 # ]
 # ///
-import glob
-import sys
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pltpublish as pub
 import seaborn as sns
 from scipy.integrate import trapezoid as simpson
-import numpy as np
 
 ACCEPTED_SOLVERS = sorted(["bs", "MILP", "rs"])
 
+plt.rcParams["text.usetex"] = True
+
+
+def prefix(name: str) -> str:
+    return r"DCC\textsubscript{\scriptsize\textsf{\MakeUppercase{" + name + r"}}}"
+
+
+mapping = {
+    "bs": prefix("MBENCH"),
+    "MILP": "MILP",
+    "rs": prefix("random"),
+}
 WA, WB = 1, 1
 
 
@@ -32,7 +40,10 @@ if __name__ == "__main__":
     )
     df["fraction"] = df["test"].str.split("_").str[-3]
     df = df[df["variant"].isin(ACCEPTED_SOLVERS)]
-    df = df.sort_values(by=["variant"])
+
+    df["method"] = df["variant"].replace(mapping)
+
+    df = df.sort_values(by=["method"])
 
     stratified_data = {}
 
@@ -44,20 +55,15 @@ if __name__ == "__main__":
     # test,variant,ratio,kendall
     for f in df["fraction"].unique():
         sub_df = df[df["fraction"] == f]
-
-        ax = sns.displot(data=sub_df, x="score", hue="variant", kind="ecdf")
+        ax = sns.displot(data=sub_df, x="score", hue="method", kind="ecdf")
+        ax.legend.set_frame_on(True)
         ax = plt.gca()
         for line, c, solver in zip(ax.lines, colors, ACCEPTED_SOLVERS[::-1]):
             x = line.get_xydata()[:, 0]
             y = line.get_xydata()[:, 1]
             ax.fill_between(x, y, color=c, alpha=0.3)
             auc = simpson(y[1:], x[1:])
-            print(f"{solver} AUC=", auc)
-        # plt.vlines(0.5, 0, 0.5, colors=["red"], linestyles="dashed")
-        # plt.hlines(0.5, 0, 0.5, colors=s["red"], linestyles="dashed")
         plt.xlabel("Score")
-        # plt.xlim(left=0)
-        plt.legend()
         plt.tight_layout()
 
         pub.save_fig(f"./cumulative_score_dist_{f}.png", scale=2)
